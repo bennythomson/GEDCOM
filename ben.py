662c636a8cb4a4ac47628244a06da151378caac7
mtodaro <79426579+mtodaro@users.noreply.github.com>
2021-03-01 13:02:06 -0500

    Revert "Revert "Merge branch 'main' of https://github.com/bennythomson/GEDCOM into main""
    This reverts commit 16fa48c5c353fc8c09b65833996bbbba907fa44a.

--- ben.py
+++ ben.py
@@ -1,45 +1,46 @@
 import datetime
-def marriage_before_death(conn):
+def marriage_before_death(individual=None, family=None):
     #dont fear the reaper....
+    if(individual != None and family != None):
 
-    married_after_death = []
+        #check if the death date or marriage date are none
+        if(individual[5] == None or family[1] == None):
+            return None
 
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM families")
-
-    rows = cur.fetchall()
-
-    print(rows)
-    for row in rows:
-
-        marriage_date = row[1]
-        print(marriage_date)
+        marriage_date = family[1]
+    #    print(marriage_date)
         marriage_date = datetime.datetime.strptime(marriage_date, '%Y-%m-%d').date()
+        individuals_death = datetime.datetime.strptime(individual[5], '%Y-%m-%d').date()
 
-        #print(row[3:])
-
-        for indiv in row[3:]:
-
-            indiv= indiv.strip()
-            new_cur = conn.cursor()
-
-            new_cur.execute("SELECT * FROM individuals WHERE ID = ?",(str(indiv),))
-            indiv_result = new_cur.fetchall()
+        if(individuals_death < marriage_date):
 
 
-            if(indiv_result[0][5] != None):
-                individuals_death = datetime.datetime.strptime(indiv_result[0][5], '%Y-%m-%d').date()
+            print("Error US05: " + individual[0] + " marriage after death")
+            return individual[0]
 
-                if(individuals_death < marriage_date):
-
-                    married_after_death.append(indiv)
-                    print("Error: " + indiv_result[0][0] + " marriage after death")
-
-    return married_after_death
+    return None
 
 
 
 
 
 def ben_user_stories(conn):
-    marriage_before_death(conn)
+
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM families")
+
+    rows = cur.fetchall()
+
+#    print(rows)
+    for row in rows:
+
+
+        for indiv in row[3:5]:
+            if indiv != None:
+                indiv= indiv.strip()
+                new_cur = conn.cursor()
+
+                new_cur.execute("SELECT * FROM individuals WHERE ID = ?",(str(indiv),))
+                indiv_result = new_cur.fetchall()
+
+                marriage_before_death(indiv_result[0], row)
--- tests/marriageBeforeDeathTest.py
+++ tests/marriageBeforeDeathTest.py
@@ -3,11 +3,40 @@ import sqlite3
 import sys
 sys.path.append('../')
 import ben
-
+import CS555
 class TestUserStory05(unittest.TestCase):
 
-    def testCorrectFamily(self):
-        connection = sqlite3.connect("../family.db")
+    def test01(self):
+        
+        conn = None
+
+        try:
+            conn = sqlite3.connect(':memory:')
+
+        except Error as e:
+            print(e)
+
+
+        cursor = conn.cursor()
+
+
+        #Setup the tables using the local SQL file
+        sql_file = open("../init_db.sql")
+        sql_as_string = sql_file.read()
+        cursor.executescript(sql_as_string)
+
+        #Clears the database's tables
+
+        sql = 'DELETE FROM individuals'
+        cursor.execute(sql)
+
+        sql = 'DELETE FROM families'
+        cursor.execute(sql)
+
+
+
+
+        CS555.parse_data(conn, test_ged.splitlines())
 
         lst = ben.marriage_before_death(connection)
         self.assertEqual(['I5'], lst)
