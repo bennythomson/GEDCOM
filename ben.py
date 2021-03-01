f4e575e10451ec60fe0c9ef7a6ca2119fc3cd941
mtodaro <79426579+mtodaro@users.noreply.github.com>
2021-03-01 12:45:23 -0500

    Revert "Merge branch 'main' of https://github.com/bennythomson/GEDCOM into main"
    This reverts commit 6bf770887e4879988adc546a19ce67b37bcc06a8, reversing
    changes made to 5df76f6d26c3e9edb3051ed7590a16cea6529999.

--- ben.py
+++ ben.py
@@ -1,46 +1,45 @@
 import datetime
-def marriage_before_death(individual=None, family=None):
+def marriage_before_death(conn):
     #dont fear the reaper....
-    if(individual != None and family != None):
 
-        #check if the death date or marriage date are none
-        if(individual[5] == None or family[1] == None):
-            return None
-
-        marriage_date = family[1]
-    #    print(marriage_date)
-        marriage_date = datetime.datetime.strptime(marriage_date, '%Y-%m-%d').date()
-        individuals_death = datetime.datetime.strptime(individual[5], '%Y-%m-%d').date()
-
-        if(individuals_death < marriage_date):
-
-
-            print("Error US05: " + individual[0] + " marriage after death")
-            return individual[0]
-
-    return None
-
-
-
-
-
-def ben_user_stories(conn):
+    married_after_death = []
 
     cur = conn.cursor()
     cur.execute("SELECT * FROM families")
 
     rows = cur.fetchall()
 
-#    print(rows)
+    print(rows)
     for row in rows:
 
+        marriage_date = row[1]
+        print(marriage_date)
+        marriage_date = datetime.datetime.strptime(marriage_date, '%Y-%m-%d').date()
 
-        for indiv in row[3:5]:
-            if indiv != None:
-                indiv= indiv.strip()
-                new_cur = conn.cursor()
+        #print(row[3:])
 
-                new_cur.execute("SELECT * FROM individuals WHERE ID = ?",(str(indiv),))
-                indiv_result = new_cur.fetchall()
+        for indiv in row[3:]:
 
-                marriage_before_death(indiv_result[0], row)
+            indiv= indiv.strip()
+            new_cur = conn.cursor()
+
+            new_cur.execute("SELECT * FROM individuals WHERE ID = ?",(str(indiv),))
+            indiv_result = new_cur.fetchall()
+
+
+            if(indiv_result[0][5] != None):
+                individuals_death = datetime.datetime.strptime(indiv_result[0][5], '%Y-%m-%d').date()
+
+                if(individuals_death < marriage_date):
+
+                    married_after_death.append(indiv)
+                    print("Error: " + indiv_result[0][0] + " marriage after death")
+
+    return married_after_death
+
+
+
+
+
+def ben_user_stories(conn):
+    marriage_before_death(conn)
